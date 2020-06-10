bb96dafe54164a460125c478c82e6c215db8db31
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2020-06-10 13:26:04 +0200

    Python script to add field outputs to an ODB database

---
+++ field_outputs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python2
+# -*- coding: iso-8859-1 -*-
+
+import csv
+import io
+import odbAccess
+
+ODB_PATH = "Z:\\database.odb"
+
+ODB_STEP = "Example-Step"
+
+ODB_FRAME = -1
+
+ODB_INSTANCE = "EXAMPLE-INSTANCE"
+
+CSV_PATH = "Z:\\enriched.csv"
+
+odb = odbAccess.openOdb(ODB_PATH)
+frame = odb.steps[ODB_STEP].frames[ODB_FRAME]
+instance = odb.rootAssembly.instances[ODB_INSTANCE]
+
+csvfile = open(CSV_PATH)
+reader = csv.reader(csvfile)
+names = next(reader)
+csv_data = [[] for _ in names]
+for row in reader:
+  for i in range(len(row)):
+    if i == 0:
+      csv_data[i].append(int(row[i]))
+    else:
+      csv_data[i].append((float(row[i]),))
+csvfile.close()
+
+for i, name in enumerate(names):
+  if i == 0: continue
+  if name in frame.fieldOutputs.keys(): continue
+
+  new_field_output = frame.FieldOutput(name=name, description=name, type=odbAccess.SCALAR)
+  new_field_output.addData(
+    position = odbAccess.NODAL,
+    instance = instance,
+    labels = csv_data[0],
+    data = csv_data[i])
+
+odb.save()
+odb.close()
